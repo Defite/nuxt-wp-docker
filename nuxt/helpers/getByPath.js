b0b28f7549276674d1781dce457bc2fc94240358
Nikita Makhov <defite@gmail.com>
2019-03-18 16:04:23 +0300

    getByPath refactoring

--- nuxt/helpers/getByPath.js
+++ nuxt/helpers/getByPath.js
@@ -1,21 +1,7 @@
-const getByPath = function (props, object) {
-    if (typeof props === 'string') {
-        props = props.split('.');
-    }  
-      
-    if (object === undefined || object === null) {
-      return;
-    }
-    
-    if (props.length === 0) {
-      return object;
-    }
-    
-    const foundedProps = object[props[0]];
-    const remainingProps = props.slice(1);
-    
-    return getByPath(remainingProps, foundedProps);
-      
+const getByPath = function (path, obj) {
+    return (new String(path)).split('.').reduce((res, prop) => {
+        return res && res[prop];
+    }, obj);
 }
 
 export default getByPath;
